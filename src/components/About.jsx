8dcc3bf98ef10f5a4265d7b27d51c88b766e2a8e
nidinbose <nidinbose700@gmail.com>
2024-10-04 23:49:11 +0530

    adding

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -56,7 +56,7 @@ const About = () => {
         <h1 className="text-left text-5xl font-mono hover:text-emerald-400 xl:pr-[670px] mb-20 font-bold text-white">
           Abo<span className="text-emerald-400 hover:text-red-500 mb-20 font-bold">ut</span> me
         </h1>
-        <Tabs className="flex flex-col xl:flex-row gap-12">
+        <Tabs className="flex flex-col xl:flex-row gap-[290px]">
           <TabList className="flex flex-col w-full max-w-[380px] mx-auto xl:mx-0 gap-6">
             <Tab className="text-white hover:bg-emerald-400 hover:text-gray-700 font-bold font-mono rounded-md pb-1 pt-1 bg-white/10 pt-2 pb-2 text-center">Experience</Tab>
             <Tab className="text-white hover:bg-emerald-400 hover:text-gray-700 font-bold font-mono rounded-md pb-1 pt-1 bg-white/10 pt-2 pb-2 text-center">Education</Tab>
@@ -64,74 +64,55 @@ const About = () => {
             <Tab className="text-white hover:bg-emerald-400 hover:text-gray-700 font-bold font-mono rounded-md pb-1 pt-1 bg-white/10 pt-2 pb-2  text-center">About me</Tab>
           </TabList>
 
-          <div className='min-h-[] w-full'>
-         
-
-                    <TabPanel className="h-96 w-full overflow-y-scroll gap-5">
-              <div className="bg-white/10 backdrop-blur-md p-6 rounded-lg shadow-lg border border-white/20">
-                <h2 className="text-2xl font-bold mb-4 text-emerald-400 font-mono">{experience.title}</h2>
-                <p className="mb-4 text-white/60 hover:text-emerald-400 font-mono">{experience.description}</p>
-                <ul className="grid grid-cols-1 gap-4">
-                  {experience.items.map((item, index) => (
-                    <li
-                      key={index}
-                      className="p-4 bg-white/10 rounded-lg border border-white/20 text-white hover:text-emerald-400 shadow-lg transition hover:scale-105"
-                    >
-                      <strong className="block text-lg">{item.company}</strong>
-                      <span>{item.position}</span> <br />
-                      <span className="text-sm text-gray-300">({item.duration})</span>
-                    </li>
-                  ))}
-                </ul>
-              </div>
+          <div className='min-h-[70vh] w-full'>
+            {/* Experience Tab Panel */}
+            <TabPanel>
+              <h2 className="text-2xl font-bold mb-4 font-mono text-emerald-400">{experience.title}</h2>
+              <p className="mb-4 text-white hover:text-white/60">{experience.description}</p>
+              <ul className="space-y-4">
+                {experience.items.map((item, index) => (
+                  <li key={index} className='text-white hover:text-emerald-400'>
+                    <strong>{item.company}</strong> - {item.position} ({item.duration})
+                  </li>
+                ))}
+              </ul>
             </TabPanel>
 
             {/* Education Tab Panel */}
-            <TabPanel className="h-96 w-full overflow-y-scroll">
-              <div className="bg-white/10 backdrop-blur-md p-6 rounded-lg shadow-lg border border-white/20">
-                <h2 className="text-2xl font-bold mb-4 text-emerald-400 font-mono">{education.title}</h2>
-                <p className="mb-4 text-white/60 hover:text-emerald-400 font-mono">{education.description}</p>
-                <ul className="grid grid-cols-1 gap-4">
-                  {education.items.map((item, index) => (
-                    <li
-                      key={index}
-                      className="p-4 bg-white/10 rounded-lg border border-white/20 text-white hover:text-emerald-400 shadow-lg transition hover:scale-105"
-                    >
-                      <strong className="block text-lg">{item.institution}</strong>
-                      <span>{item.degree}</span> <br />
-                      <span className="text-sm text-gray-300">({item.duration})</span>
-                    </li>
-                  ))}
-                </ul>
-              </div>
+            <TabPanel>
+              <h2 className="text-2xl font-bold mb-4 text-emerald-400 font-mono">{education.title}</h2>
+              <p className="mb-4 text-white/60 hover:text-emerald-400 font-mono">{education.description}</p>
+              <ul className="space-y-4">
+                {education.items.map((item, index) => (
+                  <li key={index} className='text-white hover:text-emerald-400'>
+                    <strong>{item.institution}</strong> - {item.degree} ({item.duration})
+                  </li>
+                ))}
+              </ul>
             </TabPanel>
 
             {/* Skills Tab Panel */}
             <TabPanel>
-              <div className="bg-white/10 backdrop-blur-md p-6 rounded-lg shadow-lg border border-white/20">
-                <h2 className="text-2xl font-bold mb-4 text-emerald-400 font-mono">{skills.title}</h2>
-                <p className="mb-4 text-white hover:text-white/60">{skills.description}</p>
-                <ul className="grid grid-cols-2 gap-4">
-                  {skills.items.map((skill, index) => (
-                    <li key={index} className='text-white hover:text-emerald-400'>{skill}</li>
-                  ))}
-                </ul>
-              </div>
+              <h2 className="text-2xl font-bold mb-4 text-emerald-400 font-mono">{skills.title}</h2>
+              <p className="mb-4 text-white hover:text-white/60">{skills.description}</p>
+              <ul className="space-y-2">
+                {skills.items.map((skill, index) => (
+                  <li key={index} className='text-white hover:text-emerald-400'>{skill}</li>
+                ))}
+              </ul>
             </TabPanel>
 
             {/* About Me Tab Panel */}
             <TabPanel>
-              <div className="bg-white/10 backdrop-blur-md p-6 rounded-lg shadow-lg border border-white/20">
-                <h2 className="text-2xl font-bold mb-4 text-emerald-400 font-mono">{about.title}</h2>
-                <p className="mb-4 text-white hover:text-white/60">{about.description}</p>
-                <ul className="space-y-4">
-                  {about.info.map((info, index) => (
-                    <li key={index} className='text-white hover:text-emerald-400'>
-                      <strong>{info.fieldName}</strong>: {info.fieldValue}
-                    </li>
-                  ))}
-                </ul>
-              </div>
+              <h2 className="text-2xl font-bold mb-4 text-emerald-400 font-mono">{about.title}</h2>
+              <p className="mb-4 text-white hover:text-white/60">{about.description}</p>
+              <ul className="space-y-4">
+                {about.info.map((info, index) => (
+                  <li key={index} className='text-white hover:text-emerald-400'>
+                    <strong>{info.fieldName}</strong>: {info.fieldValue}
+                  </li>
+                ))}
+              </ul>
             </TabPanel>
           </div>
         </Tabs>
